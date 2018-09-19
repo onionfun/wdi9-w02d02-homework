e4ef3d130b038026ab98976aac5e04abdfbf5dd0
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2018-09-19 08:58:34 -0600

    part 1

---
+++ javascript/app7.js
@@ -0,0 +1,68 @@
+[
+    {
+      name: "Bulbasaur",
+      damage: 60
+    }, {
+      name: "Caterpie",
+      damage: 40
+    }, {
+      name: "Charmander",
+      damage: 60
+    }, {
+      name: "Clefairy",
+      damage: 50
+    }, {
+      name: "Jigglypuff",
+      damage: 60
+    }, {
+      name: "Mankey",
+      damage: 30
+    }, {
+      name: "Meowth",
+      damage: 60
+    }, {
+      name: "Nidoran - female",
+      damage: 60
+    }, {
+      name: "Nidoran - male",
+      damage: 50
+    }, {
+      name: "Oddish",
+      damage: 40
+    }, {
+      name: "Pidgey",
+      damage: 50
+    }, {
+      name: "Pikachu",
+      damage: 50
+    }, {
+      name: "Poliwag",
+      damage: 50
+    }, {
+      name: "Psyduck",
+      damage: 60
+    }, {
+      name: "Rattata",
+      damage: 30
+    }, {
+      name: "Squirtle",
+      damage: 60
+    }, {
+      name: "Vulpix",
+      damage: 50
+    }, {
+      name: "Weedle", 
+      damage: 40
+    }
+  ]
+
+//   The basic idea
+// You are going to create a simple card game in which a player will be 
+// able to battle the autoplayer. The game will deal 3 cards (each of which 
+//has a damage value) to the player and three cards to the autoplayer. 
+//The player will choose a card, and the computer will randomly choose 
+//a card, and whichever's card has the highest value will win the point.
+//A round is finished once this has happened three times.
+// Hint/reminder: use a property when you want to "keep track" of something
+// Or should you create a method?
+// Hint/reminder: use a method when you want to "do" something
